5ebb5f273396fdbc5e87ed9e8e7ea6d0daab0106
Ryan Palmer <rjpalmer9888@gmail.com>
2021-06-24 09:31:56 -0500

    Added Identity Samples, Imported PSv1

--- PersonalSiteMVC/Controllers/HomeController.cs
+++ PersonalSiteMVC/Controllers/HomeController.cs
@@ -1,28 +1,44 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
 namespace PersonalSiteMVC.Controllers
 {
     public class HomeController : Controller
     {
+        [HttpGet]
         public ActionResult Index()
         {
             return View();
         }
 
+        [HttpGet]
+        //[Authorize]
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "Your app description page.";
 
             return View();
         }
 
+        [HttpGet]
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Links()
+        {
+            ViewBag.Message = "Your links page.";
+
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Post()
+        {
+            ViewBag.Message = "Your post page.";
 
             return View();
         }
--- PersonalSiteMVC/Global.asax.cs
+++ PersonalSiteMVC/Global.asax.cs
@@ -1,13 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using PersonalSiteMVC.Models;
+using System.Data.Entity;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
 
 namespace PersonalSiteMVC
 {
+    // Note: For instructions on enabling IIS7 classic mode, 
+    // visit http://go.microsoft.com/?LinkId=301868
     public class MvcApplication : System.Web.HttpApplication
     {
         protected void Application_Start()
